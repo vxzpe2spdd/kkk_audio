eb82a87504cef5fd9f567a1f92f385994d67c2b4
vxzpe2spdd <57292988+vxzpe2spdd@users.noreply.github.com>
2023-12-05 11:51:08 +0300

    Attempted another arguments for silence.

--- main.py
+++ main.py
@@ -28,12 +28,7 @@ API_HASH=sys.argv[1:][5]
 app = Client("my_account", API_ID, API_HASH)
 
 def remove_silence(filename, output):
-    silence_args = 'start_periods=0:' \
-        'start_duration=0:' \
-        'start_threshold=0:' \
-        'stop_periods=-1:' \
-        'stop_duration=5:' \
-        'stop_threshold=-45dB';
+    silence_args = '1:5:-45dB';
     bundle = (
         ffmpeg
         .input(filename)
