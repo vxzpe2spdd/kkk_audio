82d6169f78bb5a46caa290758a838182d94b06bf
vxzpe2spdd <VxZpE2SPDD@vivaldi.net>
2023-12-06 05:58:34 +0300

    ,

--- main.py
+++ main.py
@@ -152,7 +152,7 @@ def run_ffmpeg(filename, cut_start_str, out):
     cmd = f'{ffmpeg_exec} -i {filename} -b:a 128K -vn {silence} -ss {cut_start_str} {out}';
     os.system(cmd);
 
-def download_tag_upload(url, title, date_str, season, episode, cut_start_str):
+def download_tag_upload(urls, title, date_str, season, episode, cut_start_str):
     def nice_title():
         episode_str = "{:03d}".format(episode);
         space = ' ' if (len(title) > 0) else '';
@@ -161,18 +161,18 @@ def download_tag_upload(url, title, date_str, season, episode, cut_start_str):
 
     nice_name = f'{artist_name} — {title}.mp3';
     temp_name = 'out.mp3';
-    if type(url) is list and len(url) == 2:
-        u1 = url[0];
-        u2 = url[1];
+    if type(urls) is list and len(urls) == 2:
+        u1 = urls[0];
+        u2 = urls[1];
         f1 = download_single_vk(u1) if ('vk.com' in u1) else download_single(u1);
         f2 = download_single_vk(u2) if ('vk.com' in u2) else download_single(u2);
         run_ffmpeg(f1, cut_start_str, 'out1.mp3');
         run_ffmpeg(f2, '00:00', 'out2.mp3');
         os.system(f'{ffmpeg_exec} -f concat -i list_to_concat.txt -c copy {temp_name}');
-    elif type(url) is list and len(url) == 3:
-        u1 = url[0];
-        u2 = url[1];
-        u3 = url[2];
+    elif type(urls) is list and len(urls) == 3:
+        u1 = urls[0];
+        u2 = urls[1];
+        u3 = urls[2];
         f1 = download_single_vk(u1) if ('vk.com' in u1) else download_single(u1);
         f2 = download_single_vk(u2) if ('vk.com' in u2) else download_single(u2);
         f3 = download_single_vk(u3) if ('vk.com' in u3) else download_single(u3);
@@ -180,11 +180,11 @@ def download_tag_upload(url, title, date_str, season, episode, cut_start_str):
         run_ffmpeg(f2, '00:00', 'out2.mp3');
         run_ffmpeg(f3, '00:00', 'out3.mp3');
         os.system(f'{ffmpeg_exec} -f concat -i list_to_concat_three.txt -c copy {temp_name}');
-    elif type(url) is list and len(url) == 4:
-        u1 = url[0];
-        u2 = url[1];
-        u3 = url[2];
-        u4 = url[3];
+    elif type(urls) is list and len(urls) == 4:
+        u1 = urls[0];
+        u2 = urls[1];
+        u3 = urls[2];
+        u4 = urls[3];
         f1 = download_single_vk(u1) if ('vk.com' in u1) else download_single(u1);
         f2 = download_single_vk(u2) if ('vk.com' in u2) else download_single(u2);
         f3 = download_single_vk(u3) if ('vk.com' in u3) else download_single(u3);
@@ -195,20 +195,21 @@ def download_tag_upload(url, title, date_str, season, episode, cut_start_str):
         run_ffmpeg(f4, '00:00', 'out4.mp3');
         os.system(f'{ffmpeg_exec} -f concat -i list_to_concat_four.txt -c copy {temp_name}');
     else:
-        file_name = download_single_vk(url) if ('vk.com' in url) else download_single(url);
+        u1 = urls[0];
+        file_name = download_single_vk(u1) if ('vk.com' in u1) else download_single(u1);
         run_ffmpeg(file_name, cut_start_str, temp_name);
     # trim_audio(file_name, cut_start_str);
     # temp_name = remove_silence(file_name, "temp" + file_name);
 
     timestamp = time.mktime(time.strptime(date_str, MY_FORMAT))
     os.utime(temp_name, (int(timestamp), int(timestamp)))
-    os.rename(temp_name, file_name);
+    # os.rename(temp_name, file_name);
 
     total_in_season = 0;
     if (season == 2):
         total_in_season = 71
 
-    file_non_tagged = eyed3.load(file_name);
+    file_non_tagged = eyed3.load(temp_name);
     # file_non_tagged.tag = eyed3.load(download_last_tagged_audio()).tag;
     file_non_tagged.tag._setDiscNum(season);
     file_non_tagged.tag._setTrackNum((episode, total_in_season));
@@ -228,8 +229,8 @@ def download_tag_upload(url, title, date_str, season, episode, cut_start_str):
 
     file_non_tagged.tag.save();
 
-    cover_file_name = extract_cover(file_name);
-    time_secs = duration_seconds(file_name);
+    cover_file_name = extract_cover(temp_name);
+    time_secs = duration_seconds(temp_name);
 
     print("Uploading...");
     print(f'audio file = {nice_name}');
@@ -238,7 +239,7 @@ def download_tag_upload(url, title, date_str, season, episode, cut_start_str):
     print(f'title = {file_non_tagged.tag.title}');
     print(f'cover_file_name = {cover_file_name}');
 
-    os.rename(file_name, nice_name);
+    os.rename(temp_name, nice_name);
 
     with app:
         app.send_audio(
@@ -267,110 +268,480 @@ def check_is_video_good(videoId):
           return False;
     return False;
 
-episode_counter = 1;
-download_tag_upload(
-    # 'https://vk.com/video-72495291_456239592', # Test.
-    'https://vk.com/video-72495291_456239518',
-    'Сценарий к фильму',
-    '13.12.2016',
-    2,
-    episode_counter,
-    '02:15');
-episode_counter = episode_counter + 1;
 
-download_tag_upload(
-    ['https://vk.com/video-72495291_456239538', 'https://vk.com/video-72495291_456239539'],
+episode_counter = 1;
+def second_season(urls, title, date_str, cut_start_str):
+    download_tag_upload(
+        list(map((lambda s : 'https://vk.com/video-' + s), urls)),
+        title,
+        date_str,
+        2,
+        episode_counter,
+        cut_start_str);
+    episode_counter = episode_counter + 1;
+
+# second_season(
+#     # ['72495291_456239592'], # Test.
+#     ['72495291_456239518'],
+#     'Сценарий к фильму',
+#     '13.12.2016',
+#     '02:15');
+
+second_season(
+    ['72495291_456239538', '72495291_456239539'],
     'Щячло снова на экране',
     '17.12.2016',
-    2,
-    2,
     '03:10');
-episode_counter = episode_counter + 1;
 
-download_tag_upload(
-    ['https://vk.com/video-72495291_456239565', 'https://vk.com/video-72495291_456239564'],
+second_season(
+    ['72495291_456239565', '72495291_456239564'],
     'Паром дом дом',
     '18.12.2016',
-    2,
-    episode_counter,
     '02:30');
-episode_counter = episode_counter + 1;
 
-download_tag_upload(
-    'https://vk.com/video-72495291_456239569',
+second_season(
+    ['72495291_456239569'],
     'Каикакака (КиККК)',
     '19.12.2016',
-    2,
-    episode_counter,
     '03:30');
-episode_counter = episode_counter + 1;
 
-download_tag_upload(
-    'https://vk.com/video-72495291_456239577',
+second_season(
+    ['72495291_456239577'],
     'Харкотина на еботине',
     '22.12.2016',
-    2,
-    episode_counter,
     '04:10');
-episode_counter = episode_counter + 1;
 
-download_tag_upload(
-    [
-        'https://vk.com/video-72495291_456239603',
-        'https://vk.com/video-72495291_456239601',
-        'https://vk.com/video-72495291_456239606'
-    ],
+second_season(
+    ['72495291_456239603', '72495291_456239601', '72495291_456239606'],
     'Курдючный жмых',
     '10.01.2017',
-    2,
-    episode_counter,
     '02:40');
-episode_counter = episode_counter + 1;
 
-download_tag_upload(
-    ['https://vk.com/video-72495291_456239605', 'https://vk.com/video-72495291_456239604'],
+second_season(
+    ['72495291_456239605', '72495291_456239604'],
     'Лилу Даллас Мульти Пасс',
     '11.01.2017',
-    2,
-    episode_counter,
     '01:56');
-episode_counter = episode_counter + 1;
 
-download_tag_upload(
-    [
-        'https://vk.com/video-72495291_456239610',
-        'https://vk.com/video-72495291_456239609',
-        'https://vk.com/video-72495291_456239608',
-        'https://vk.com/video-72495291_456239607'
-    ],
+second_season(
+    ['72495291_456239610', '72495291_456239609', '72495291_456239608', '72495291_456239607'],
     '(Кузьма) Ускорение горизонтального падения',
     '12.01.2017',
-    2,
-    episode_counter,
     '01:27');
-episode_counter = episode_counter + 1;
 
-download_tag_upload(
-    [
-        'https://vk.com/video-72495291_456239613',
-        'https://vk.com/video-72495291_456239612',
-        'https://vk.com/video-72495291_456239611'
-    ],
+second_season(
+    ['72495291_456239613', '72495291_456239612', '72495291_456239611'],
     'Последний довод бегемотика',
     '13.01.2017',
-    2,
-    episode_counter,
     '08:40');
-episode_counter = episode_counter + 1;
 
-download_tag_upload(
-    'https://vk.com/video-72495291_456239618',
+second_season(
+    ['72495291_456239618'],
     'Бертольдт Брехун',
     '15.01.2017',
-    2,
-    episode_counter,
     '02:10');
-episode_counter = episode_counter + 1;
+
+second_season(
+    ['72495291_456239620', '72495291_456239619'],
+    '(Кузьма) Вовремя',
+    '16.01.2017',
+    '01:35');
+
+second_season(
+    ['72495291_456239623', '72495291_456239622', '72495291_456239621'],
+    'Полюсы Хуато',
+    '16.01.2017',
+    '02:42');
+
+second_season(
+    ['72495291_456239631', '72495291_456239630', '72495291_456239628'],
+    'Рубаха парень и Штаны девушка',
+    '18.01.2017',
+    '03:16');
+
+second_season(
+    ['72495291_456239635', '72495291_456239633', '72495291_456239632'],
+    'Мистер Крабапл',
+    '20.01.2017',
+    '01:51');
+
+second_season(
+    ['72495291_456239639', '72495291_456239638', '72495291_456239637'],
+    'Итоги киногода 2016',
+    '21.01.2017',
+    '03:16');
+
+second_season(
+    ['72495291_456239641', '72495291_456239640'],
+    'Итоги киногода 2016 - 2',
+    '22.01.2017',
+    '02:48');
+
+second_season(
+    ['72495291_456239643', '72495291_456239642'],
+    'Квадрат бифуркации',
+    '23.01.2017',
+    '05:46');
+
+second_season(
+    ['72495291_456239645', '72495291_456239644'],
+    'Сердоболька',
+    '24.01.2017',
+    '01:53');
+
+second_season(
+    ['72495291_456239647', '72495291_456239646'],
+    'Среда',
+    '25.01.2017',
+    '00:31');
+
+second_season(
+    ['72495291_456239655', '72495291_456239653'],
+    'Путешествия с мёртвым индейцем',
+    '26.01.2017',
+    '02:54');
+
+second_season(
+    ['72495291_456239651', '72495291_456239650'],
+    'Трижды двадцать и десять',
+    '27.01.2017',
+    '01:46');
+
+second_season(
+    ['72495291_456239660', '72495291_456239659'],
+    'Флюгегехаймен',
+    '29.01.2017',
+    '04:25');
+
+second_season(
+    ['72495291_456239664', '72495291_456239663'],
+    'Вакуум высокой плотности',
+    '30.01.2017',
+    '10:07');
+
+second_season(
+    ['72495291_456239667'],
+    'Сол Беллоу - На память обо мне',
+    '31.01.2017',
+    '02:45');
+
+second_season(
+    ['72495291_456239669', '72495291_456239668'],
+    'Штурмбанфюррер ГеоГёссер пожаловал в резиденцию',
+    '01.02.2017',
+    '09:00');
+
+second_season(
+    ['72495291_456239671'],
+    'Дятл Перевалова',
+    '02.02.2017',
+    '02:56');
+
+second_season(
+    ['72495291_456239675'],
+    'Кирибатти',
+    '07.02.2017',
+    '08:52');
+
+second_season(
+    ['72495291_456239678', '72495291_456239677'],
+    'И немедленно вы...вих',
+    '08.02.2017',
+    '01:55');
+
+second_season(
+    ['72495291_456239683', '72495291_456239682'],
+    'УВNДЕВШNЕ ЭТО SОЙДYТ CУМА',
+    '09.02.2017',
+    '05:55');
+
+second_season(
+    ['72495291_456239686', '72495291_456239685'],
+    'Сол Беллоу - Севший в калошу',
+    '10.02.2017',
+    '01:58');
+
+second_season(
+    ['72495291_456239687'],
+    'Ебловатый и Дебелый снова в седле',
+    '11.02.2017',
+    '04:32');
+
+second_season(
+    ['72495291_456239689', '72495291_456239688'],
+    'Коллаборационистское правительство',
+    '13.02.2017',
+    '06:01');
+
+second_season(
+    ['72495291_456239691', '72495291_456239690'],
+    'Такое же название',
+    '14.02.2017',
+    '03:15');
+
+second_season(
+    ['72495291_456239696'],
+    'Юбилейный стрим',
+    '16.02.2017',
+    '04:44');
+
+second_season(
+    ['72495291_456239704', '72495291_456239703'],
+    'ВЖОП и СРАК',
+    '19.02.2017',
+    '03:10');
+
+second_season(
+    ['72495291_456239707'],
+    'Трубокур',
+    '20.02.2017',
+    '00:01');
+
+second_season(
+    ['72495291_456239715', '72495291_456239714'],
+    'Барокко рокко ко', # episode 30
+    '21.02.2017',
+    '03:14');
+
+second_season(
+    ['72495291_456239717', '72495291_456239716'],
+    'sha lava LAVA LAAVAAAAA!!!!!',
+    '22.02.2017',
+    '07:13');
+
+second_season(
+    ['72495291_456239734'],
+    'Второе кедровое воскурение',
+    '24.02.2017',
+    '00:01');
+
+second_season(
+    ['72495291_456239736', '72495291_456239735'],
+    'Толстожопое лобби',
+    '25.02.2017',
+    '02:45');
+
+second_season(
+    ['72495291_456239739', '72495291_456239735'],
+    'Наивно это и смешно, но так легко',
+    '26.02.2017',
+    '02:10');
+
+second_season(
+    ['72495291_456239742', '72495291_456239741'],
+    'Акведук, Теремок и Две Прищепки',
+    '27.02.2017',
+    '01:20');
+
+second_season(
+    ['72495291_456239743'],
+    'Ла-Ла Леунный свет',
+    '27.02.2017',
+    '02:46');
+
+second_season(
+    ['72495291_456239746'],
+    'Лазерная коррекция жопы',
+    '28.02.2017',
+    '07:16');
+
+second_season(
+    ['72495291_456239749'],
+    'Реструктуризация точек схлопывания',
+    '02.03.2017',
+    '07:02');
+
+second_season(
+    ['72495291_456239752'],
+    'Последняя трансляция...',
+    '03.03.2017',
+    '06:10');
+
+second_season(
+    ['72495291_456239756'],
+    'Лобко лобко',
+    '04.03.2017',
+    '05:58');
+
+second_season(
+    ['72495291_456239763'],
+    'Что за стрим?',
+    '05.03.2017',
+    '12:03');
+
+second_season(
+    ['72495291_456239767'],
+    'Приглашаются Венеруны',
+    '06.03.2017',
+    '17:55');
+
+second_season(
+    ['72495291_456239770'],
+    'Ж-Корвалол и его друзья Пестициды',
+    '07.03.2017',
+    '08:50');
+
+second_season(
+    ['72495291_456239771'],
+    'Ромовая баба, пепсиколовый мужик',
+    '08.03.2017',
+    '06:50');
+
+second_season(
+    ['72495291_456239775'],
+    'Ещё один день в раю',
+    '09.03.2017',
+    '03:44');
+
+second_season(
+    ['72495291_456239778'],
+    'Атеисты седьмого дня',
+    '10.03.2017',
+    '03:04');
+
+second_season(
+    ['72495291_456239785'],
+    'Сильвупле, печёночник',
+    '12.03.2017',
+    '09:27');
+
+second_season(
+    ['72495291_456239792'],
+    'Эрик и Мария Ремарки представляют вашему вниманию Кульбиты',
+    '15.03.2017',
+    '10:53');
+
+second_season(
+    ['72495291_456239798'],
+    'Программа по защите свидетелей Иегова',
+    '16.03.2017',
+    '07:27');
+
+second_season(
+    ['72495291_456239806'],
+    '(Дарья Зарыковская) Париж, париж Ибонжур',
+    '18.03.2017',
+    '01:37');
+
+second_season(
+    ['72495291_456239819', '72495291_456239818'],
+    'Перекись населения',
+    '24.03.2017',
+    '06:37');
+
+second_season(
+    ['72495291_456239824'],
+    'Музы Кабатуева',
+    '25.03.2017',
+    '05:00');
+
+second_season(
+    ['72495291_456239825'],
+    'Педрильный клуб любителей пощекотать очко',
+    '26.03.2017',
+    '04:50');
+
+second_season(
+    ['72495291_456239831'],
+    'Кесарево Свечение Чистого Разума',
+    '28.03.2017',
+    '06:30');
+
+second_season(
+    ['72495291_456239837'],
+    'Иж Юпитер и Sega Saturn', # epi 56
+    '29.03.2017',
+    '02:08');
+
+second_season(
+    ['72495291_456239840'],
+    'Элитный стрим для солидных господ',
+    '30.03.2017',
+    '01:21');
+
+second_season(
+    ['72495291_456239845'],
+    'Донч ю ноу ам локо Локо ЛОКО ЛОКОМОТИВ!!!',
+    '31.03.2017',
+    '00:01');
+
+second_season(
+    ['72495291_456239851'],
+    '(Хованский) Стрим ли вовремя? Зачем и почему?',
+    '02.04.2017',
+    '03:33');
+
+second_season(
+    ['72495291_456239852'],
+    'Чехов: Я не цитировал БигРашнБосса... но это не точно',
+    '03.04.2017',
+    '01:20');
+
+second_season(
+    ['72495291_456239855'],
+    'Лимпомор',
+    '04.04.2017',
+    '05:30');
+
+second_season(
+    ['72495291_456239858'],
+    'Первый и Второй Том... и джери',
+    '06.04.2017',
+    '07:24');
+
+second_season(
+    ['72495291_456239860', '72495291_456239859'],
+    'Уаппарата',
+    '07.04.2017',
+    '10:20');
+
+second_season(
+    ['72495291_456239866'],
+    'Акция-Акция "Бесплатная Лактация"',
+    '08.04.2017',
+    '08:15');
+
+second_season(
+    ['72495291_456239867'],
+    'Лапидарный бидалага',
+    '09.04.2017',
+    '04:28');
+
+second_season(
+    ['72495291_456239869'],
+    'Конец сезона',
+    '10.04.2017',
+    '03:30');
+
+second_season(
+    ['72495291_456239870'],
+    '(Хованский) Лобздрище и Циркулярный читают мануал',
+    '11.04.2017',
+    '03:40');
+
+second_season(
+    ['72495291_456239871'],
+    'Ноль Шесть Восемь',
+    '12.04.2017',
+    '05:25');
+
+second_season(
+    ['72495291_456239873'],
+    '(Кузьма) Румынская тяга к знаниям',
+    '13.04.2017',
+    '00:01');
+
+second_season(
+    ['72495291_456239874'],
+    'Совершенно новый формат А4',
+    '14.04.2017',
+    '00:01');
+
+second_season(
+    ['72495291_456239875'],
+    'Никогда такого не было, и вот опять! (БАН на ЮТУБЕ)',
+    '15.04.2017',
+    '00:01');
 
 # yt_entries = find_not_uploaded(read_last_messages());
 # for yt_entry in reversed(yt_entries):
